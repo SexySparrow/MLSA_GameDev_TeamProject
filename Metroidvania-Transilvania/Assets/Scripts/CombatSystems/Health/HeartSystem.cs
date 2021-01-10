6c00b55616846b6359946cfca30191e2cc793c22
Brabete Adrian <constantin.brabete@stud.acs.upb.ro>
2021-01-10 20:49:33 +0200

    reorg the scripts folder

---
+++ Metroidvania-Transilvania/Assets/Scripts/CombatSystems/Health/HeartSystem.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HeartSystem : MonoBehaviour
+{
+    public GameObject[] hearts;
+    private int life;
+    int initialLife;
+
+    
+    private void Start()
+    {
+        life = hearts.Length;
+        initialLife = hearts.Length;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+          
+    }
+
+    public void TakeDamage(int d)
+    {
+        life -= d;
+        hearts[life].GetComponent<Image>().enabled = false;
+    }
+
+    public void GetHealth(int h)
+    {
+        if (life < initialLife)
+        {
+            life += h;
+            hearts[life - 1].GetComponent<Image>().enabled = true;
+        }
+        else if (life >= initialLife) Console.WriteLine("Full HP");
+        
+    }
+}
